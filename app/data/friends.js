25ba25d7111bdf69db0e7c9f064b71ee3568066d
Okyfunoky <okyfunoky@hotmail.com>
2019-10-04 19:47:37 -0700

    Added images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,28 @@
 var friends = [
-    {name: "Captain America", scores:[5,1,1,4,5,5]},
-    {name: "Black Widow", scores:[3,1,5,2,4,2]},
-    {name: "Iron Man", scores:[3,5,1,2,1,2]},
-    {name: "Thor", scores:[3,1,1,5,3,5]},
+    {
+        name: "Captain America",
+        scores: [5, 1, 1, 4, 5, 5],
+        imageSource: "https://fsmedia.imgix.net/11/91/65/66/aa08/4ef6/be12/964b648601b6/captain-america-the-winter-soldier.jpeg?rect=0%2C0%2C1920%2C960&auto=format%2Ccompress&dpr=2&w=650",
+        alt: "Captain America - First Avenger"
+    },
+    {
+        name: "Black Widow",
+        scores: [3, 1, 5, 2, 4, 2],
+        imageSource: "https://i2-prod.mirror.co.uk/incoming/article16339791.ece/ALTERNATES/s615/0_black-widow.jpg",
+        alt: "Black Widow - deadly assasin"
+    },
+    {
+        name: "Iron Man",
+        scores: [3, 5, 1, 2, 1, 2],
+        imageSource: "https://cdn.pinkvilla.com/files/styles/contentpreview/public/iron-man-phase-4_0.jpg?itok=zzbN51r7",
+        alt: "Iron Man - rogue with a heart of energy"
+    },
+    {
+        name: "Thor",
+        scores: [3, 1, 1, 5, 3, 5],
+        imageSource: "https://www.screengeek.net/wp-content/uploads/2019/04/thor-4-chris-hemsworth.jpg",
+        alt: "Thor - strongest Avenger"
+    }
 ]
 
 module.exports = friends;
